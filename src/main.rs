e05262251c15b2cd5881d28dcbd7e800f0a5d47e
superlou <lousimons@gmail.com>
2022-05-24 20:16:32 -0400

    Not sure what I'm doing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use lapce_plugin::{register_plugin, start_lsp, LapcePlugin};
+use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
+
+#[derive(Default)]
+struct State {}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct PluginInfo {
+    arch: String,
+    os: String,
+    configuration: Configuration,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Configuration {
+    language_id: String,
+    options: Option<Value>,
+}
+
+register_plugin!(State);
+
+impl LapcePlugin for State {
+    fn initialize(&mut self, info: serde_json::Value) {
+        let info = serde_json::from_value::<PluginInfo>(info).unwrap();
+        let arch = match info.arch.as_str() {
+            "x86_64" => "x86_64",
+            "aarch64" => "aarch64",
+            _ => return,
+        };
+        let os = match info.os.as_str() {
+            "linux" => "unknown-linux-gnu",
+            "macos" => "apple-darwin",
+            "windows" => "pc-windows-msvc",
+            _ => return,
+        };
+
+        let file_name = String::from("jedi-language-server");
+
+        start_lsp(&file_name, "rust", info.configuration.options);
+    }
 }
