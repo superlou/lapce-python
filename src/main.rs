c06b31b73d6558a41440f100dbc0cf51ec93731c
superlou <lousimons@gmail.com>
2022-12-16 20:55:36 -0500

    Removed old comments

--- src/main.rs
+++ src/main.rs
@@ -63,34 +63,3 @@ impl LapcePlugin for State {
         }
     }
 }
-
-// impl LapcePlugin for State {
-//     fn initialize(&mut self, info: serde_json::Value) {
-//         let info = serde_json::from_value::<PluginInfo>(info).unwrap();
-//         let _arch = match info.arch.as_str() {
-//             "x86_64" => "x86_64",
-//             "aarch64" => "aarch64",
-//             _ => return,
-//         };
-//         let _os = match info.os.as_str() {
-//             "linux" => "unknown-linux-gnu",
-//             "macos" => "apple-darwin",
-//             "windows" => "pc-windows-msvc",
-//             _ => return,
-//         };
-
-//         let (exec_path, use_system_lsp) = match &info.configuration.lsp_exec {
-//             Some(path) => (path.as_str(), true),
-//             None => ("pylsp", true),
-//         };
-
-//         // two copies of us are started
-//         //serde_json::to_writer_pretty(std::io::stderr(), &info).unwrap();
-//         start_lsp(
-//             exec_path,
-//             "python",
-//             info.configuration.options,
-//             use_system_lsp,
-//         );
-//     }
-// }
